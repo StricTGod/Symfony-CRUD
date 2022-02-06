1c03e99e2db6db50d72b35387c767f21a569306a
Oleksandr Malchenko <k170k89@ukr.net>
2022-02-06 23:41:26 +0200

    Initial commit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/products", name="products", methods={"GET"})
+     */
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $products = $doctrine->getRepository(Product::class)->findAll();
+
+        return $this->render('product/index.html.twig', ['products' => $products]);
+    }
+
+    /**
+     * @Route("/open-create-modal", name="open_create_modal", methods={"GET"})
+     */
+    public function openCreateModal(): Response
+    {
+        return $this->render('product/new.html.twig');
+    }
+
+    /**
+     * @Route("product/create", name="product_create", methods={"POST"})
+     */
+    public function createProduct(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $product = new Product();
+
+        $product->setName($request->get('name'));
+        $product->setPrice($request->get('price'));
+        $product->setDescription($request->get('description'));
+        $product->setIsHidden($request->get('is_hidden'));
+
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('products');
+    }
+
+    /**
+     * @Route("/open-update-modal/{id}", name="open_update_modal", methods={"GET"})
+     */
+    public function openUpdateModal(ManagerRegistry $doctrine, Request $request, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        $request->get('name');
+        $request->get('price');
+        $request->get('description');
+
+        return $this->render('product/open_update_modal.html.twig', ['product' => $product]);
+    }
+
+    /**
+     * @Route("/product-update/{id}", name="product_update", methods={"POST"})
+     */
+    public function updateProduct(ManagerRegistry $doctrine, Request $request, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        $product->setName($request->get('name'));
+        $product->setPrice($request->get('price'));
+        $product->setDescription($request->get('description'));
+
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('products');
+    }
+
+    /**
+     * @Route("/product-delete/{id}", name="product_delete", methods={"GET"})
+     */
+    public function deleteProduct(ManagerRegistry $doctrine, Request $request, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        $entityManager->remove($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('products');
+    }
+}
